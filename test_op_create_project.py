882eb6d9eb9dcebb6c91fb45da28e85d1277fc33
Whit Morriss <d.w.morriss@gmail.com>
2006-08-22 17:50:56 +0000

    rearranged and condensed test harness/fixtures into one file
    git-svn-id: https://svn.socialplanning.org/svn/topp_ftesting/branches/testbrowser_doctest@951 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- test_op_create_project.py
+++ test_op_create_project.py
@@ -1,9 +1,27 @@
 import unittest
 from zope.testing import doctest
-from Testing.ZopeTestCase import FunctionalDocFileSuite
+from Testing.ZopeTestCase import FunctionalDocFileSuite, FunctionalTestCase
 
+optionflags = doctest.REPORT_ONLY_FIRST_FAILURE | doctest.ELLIPSIS
 def test_suite():
-    print "OpenPlans test:  create project"
-    return unittest.TestSuite(
-        FunctionalDocFileSuite('create_project.txt')
-        )
+    tests = []
+    testfiles = ('login.txt',
+                 'create_project.txt',
+                 'edit_project.txt',
+                 'login.txt',
+                 'create_user.txt')
+    
+    for name in testfiles:
+        #doctest.DocFileSuite
+        tests.append(FunctionalDocFileSuite(name,
+                                            optionflags=optionflags,
+                                            package='Products.testbrowser_doctest',
+                                            test_class = FunctionalTestCase
+                                            ))
+    
+    return unittest.TestSuite(tests)
+
+if __name__ == '__main__':
+    suite = test_suite()
+    TestRunner = unittest.TextTestRunner
+    TestRunner(verbosity=1).run(suite)
