737f4c0271930a18cab8eaba9f926d5f477f56b3
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2010-06-17 16:31:37 +0000

    cleanup
    git-svn-id: https://svn.socialplanning.org/svn/flunc/branches/testing-listen@27003 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- flunc/mail.py
+++ flunc/mail.py
@@ -6,7 +6,7 @@ These extensions are pretty specific to my immediate need,
 which is writing tests for Listen.
 
 The extensions assume that the server is using 
- http://github.com/ejucovy/Products.FileWritingMailHost/
+ http://github.com/ejucovy/Products.TestMailHost/
 which writes out mails to files instead of sending them,
 and injects headers into the request to tell the client
 where to find the mails.  The expected setup for these tests
@@ -102,12 +102,10 @@ def mail_contains(value):
         raise TwillAssertionError("no match for <%s> in mail at %s" % (
                 value, mail))
 
-def send_mail(file):
+def send_mail(file, receiverURL):
     fp = open(file)
     mailStr = fp.read()
     fp.close()
-
-    receiverURL = 'http://localhost:10001/openplans/send_listen_mail'
 
     mails = send(receiverURL, mailStr)
     if mails is None: 
